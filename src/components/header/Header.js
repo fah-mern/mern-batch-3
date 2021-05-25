d923dcd4797638cc0675e248100de7fb67ed0320
Fahad-am12 <fahadkassam70@gmail.com>
2021-05-25 16:17:13 +0500

    This is my first commit and i have designed frontend with react for kfc project

---
+++ src/components/header/Header.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import logo from '../../assets/1.png'
+import './Header.css'
+import navlogo from '../../assets/cart-icon.png'
+
+ function Header() {
+    return (
+        <div className='header-wrapper'>
+            <div className='header-inner'>
+
+                <div className='item'>
+<img src={logo} alt=''/>
+                </div>
+
+                <div className='item'>
+ <ul>
+     
+ <li><a href="#">Store Location</a></li>
+ <li><a href="#">Register/Login</a></li>
+ </ul>
+                </div>
+            </div>
+
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">EVERYDAY VALUE</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link" aria-current="page" href="#">MAKE IT A MEAL</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">SIGNATURE BOXES</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">SHARING</a>
+        </li>
+      </ul>
+      <nav className="navbar navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      <img src={navlogo} alt="" width="30" height="24" className="d-inline-block align-text-top"/>
+
+    </a>
+  </div>
+</nav>
+    </div>
+  </div>
+</nav>
+            
+        </div>
+    )
+}
+
+export default Header;
